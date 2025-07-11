93c76433b3ce6535bc2d63559843ea21524e5586
Danil Davydyan <davidian654@gmail.com>
2025-07-11 11:11:53 +0300

    fix insights

--- backend/python_api/app.py
+++ backend/python_api/app.py
@@ -2,12 +2,15 @@ import os
 from uuid import uuid4
 
 from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 
 from backend.python_api.emotion_recognition_model import EmotionRecognitionModel
+from backend.python_api.mistral_model import InsightsModel
 from backend.python_api.transcription_model import TranscriptionModel
 
-emotion_recognition_model = EmotionRecognitionModel()
+insights_model = InsightsModel()
 transcription_model = TranscriptionModel()
+emotion_recognition_model = EmotionRecognitionModel()
 
 app = FastAPI(
     title="VoiceDiaryML",
@@ -25,11 +28,21 @@ async def analyze_record(file: UploadFile = File(...)) -> dict[str, str]:
         content = await file.read()
         f.write(content)
 
+    text, summary = transcription_model.get_summarization(audio_path)
+
     res = {
         "emotion": emotion_recognition_model.get_emotion(audio_path),
-        "summary": transcription_model.get_summarization(audio_path),
+        "summary": summary,
+        "text": text,
     }
 
     os.remove(audio_path)
 
     return res
+
+class Base(BaseModel):
+    text: str
+
+@app.get("/insights")
+async def get_insights(base: Base):
+    return {"insights": insights_model.analyze_text(base.text)}
