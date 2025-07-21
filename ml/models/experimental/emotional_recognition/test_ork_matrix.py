93bdef5977a12e33e4af6caba4a8231a86efcae1
ellilin <elechka.ku@gmail.com>
2025-07-21 14:26:27 +0300

    [ml]: change orch

--- ml/models/experimental/emotional_recognition/test_ork_matrix.py
+++ ml/models/experimental/emotional_recognition/test_ork_matrix.py
@@ -13,7 +13,7 @@ similarity = {
     'disgust':   {'anger': 0.9, 'disgust': 1.0, 'fear': 0.7, 'sadness': 0.8, 'joy': 0.0, 'neutral': 0.2, 'surprise': 0.5},
     'fear':      {'anger': 0.9, 'disgust': 0.6, 'fear': 1.0, 'sadness': 0.7, 'joy': 0.0, 'neutral': 0.2, 'surprise': 0.8},
     'joy':       {'anger': 0.0, 'disgust': 0.0, 'fear': 0.0, 'sadness': 0.0, 'joy': 1.0, 'neutral': 0.2, 'surprise': 0.9},
-    'neutral':   {'anger': 0.0, 'disgust': 0.6, 'fear': 0.5, 'sadness': 0.9, 'joy': 0.7, 'neutral': 1.0, 'surprise': 0.8},
+    'neutral':   {'anger': 0.0, 'disgust': 0.2, 'fear': 0.2, 'sadness': 0.2, 'joy': 0.2, 'neutral': 1.0, 'surprise': 0.2},
     'sadness':   {'anger': 0.6, 'disgust': 0.8, 'fear': 0.7, 'sadness': 1.0, 'joy': 0.0, 'neutral': 0.2, 'surprise': 0.5},
     'surprise':  {'anger': 0.6, 'disgust': 0.5, 'fear': 0.8, 'sadness': 0.0, 'joy': 0.9, 'neutral': 0.2, 'surprise': 1.0},
 }
@@ -26,8 +26,30 @@ def combine_emotions(audio_emotion: str, text_emotion: str, similarity: dict, we
         audio_score = similarity[audio_emotion][target_emotion] * weight_audio
         text_score = similarity[text_emotion][target_emotion] * weight_text
         score[target_emotion] = audio_score + text_score
-
-    return max(score, key=score.get)
+    if((audio_emotion == "neutral" or text_emotion == "neutral") and (audio_emotion != "neutral" or text_emotion != "neutral")):
+        if(audio_emotion == "neutral"):
+            return text_emotion
+        return audio_emotion
+    else:
+        return max(score, key=score.get)
 
 final_emotion = combine_emotions("Sad", "fear", similarity, weight_audio=0.4, weight_text=0.6)
-print(final_emotion)
+
+voice = ["Angry", "Disgust", "Fearful", "Happy", "Neutral", "Sad", "Surprised"]
+text = ["anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"]
+
+# Создание таблицы
+import pandas as pd
+
+table = []
+
+for v in voice:
+    row = []
+    for t in text:
+        combined = combine_emotions(v, t, similarity, weight_audio=0.4, weight_text=0.6)
+        row.append(combined)
+    table.append(row)
+
+df = pd.DataFrame(table, index=voice, columns=text)
+print(df)
+print("\n")
